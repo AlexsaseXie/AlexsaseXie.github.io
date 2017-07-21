c6b03cb80e4bb47be9f5dec406fcc0a2cb965661
AlexsaseXie <thss15_xieyw@163.com>
2017-07-21 16:33:27 +0800

    n

--- Smove/js/phoneControl.js
+++ Smove/js/phoneControl.js
@@ -29,7 +29,7 @@ var startX = 0, startY = 0;
             function touchMoveFunc(evt) {
                 try
                 {
-                    //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+                    evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
                     var touch = evt.touches[0]; //获取第一个触点
                     var x = Number(touch.pageX); //页面触点X坐标
                     var y = Number(touch.pageY); //页面触点Y坐标
